21e7bcbc8fbca133acedb4e8fb5ae83d89bca5a1
josephGuo <mailover@gmail.com>
2026-06-04 02:26:14 +0000

    Deploying to gh-pages from @ josephGuo/nav@6d8122b90ae62742943f6b1a9f0dfb6c4dc9d01e 🚀

---
+++ chunk-JL57ZYLG.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-YUBQ2J7P.js";import{ob as r,wb as e}from"./chunk-MLSPWWBS.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
